105d0df21780a8da01bbb7228a6e40be1f4d3979
aadityap0ap <adityabharti450ap@gmail.com>
2026-08-20 08:32:40 +0530

    question 1 of Introductory problem

---
+++ 01_IntroductoryProblem/01_WeiredProblem.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    long long n;
+    cin>>n;
+    vector<long long>ans;
+    //ans.push_back(n);
+    while(n != 1){
+    ans.push_back(n);
+    if(n % 2 == 0){
+        n = n / 2;
+    }
+    else{
+        n = (n * 3) + 1;
+    }
+}
+ans.push_back(1);
+for(int i = 0;i<ans.size();i++){
+    cout<<ans[i]<<" ";
+}
+return 0;
+}
